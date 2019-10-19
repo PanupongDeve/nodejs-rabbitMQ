016f677ea0c97a7ba34eb04f8572205a7d212b9b
Panupong <5635512110@psu.ac.th>
2019-10-19 15:22:34 +0700

    kearb work queues

--- src/WorkdQueues/worker.js
+++ src/WorkdQueues/worker.js
@@ -8,18 +8,25 @@ amqp.connect('amqp://localhost', function (error0, connection) {
         if (error1) {
             throw error1;
         }
-        var queue = 'hello';
+        var queue = 'task_queue';
 
         channel.assertQueue(queue, {
             durable: true
         });
 
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-        channel.consume(queue, function (msg) {
+        channel.consume(queue, function(msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+          
             console.log(" [x] Received %s", msg.content.toString());
-        }, {
+            setTimeout(function() {
+              console.log(" [x] Done");
+            }, secs * 1000);
+          }, {
+            // automatic acknowledgment mode,
+            // see https://www.rabbitmq.com/confirms.html for details
             noAck: true
-        });
+          });
     });
 
 
